6a49921455697ddd5e2dd69289149e08c67fe72b
Bruno Ribeiro <89156916+brunoribeirol@users.noreply.github.com>
2025-04-23 16:17:04 -0300

    Add files via upload

---
+++ src/main/java/com/cesar/school/JiraGamificado/JiraGamificadoApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.school.JiraGamificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraGamificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraGamificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesar/school/JiraGamificado/JiraGamificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar.school.JiraGamificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraGamificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
