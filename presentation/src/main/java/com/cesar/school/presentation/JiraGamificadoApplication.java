7a1a54c7a73e7f46ec83d0792abd9603ca81d11f
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2025-05-17 01:45:13 -0300

    feat(presentation): add Spring Boot application startup class

---
+++ presentation/src/main/java/com/cesar/school/presentation/JiraGamificadoApplication.java
@@ -0,0 +1,21 @@
+package com.cesar.school.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EntityScan(basePackages = {
+        "com.cesar.school.infrastructure.persistence.entity"
+})
+@ComponentScan(basePackages = {
+        "com.cesar.school.application",
+        "com.cesar.school.infrastructure.configuration",
+        "com.cesar.school.presentation"
+})
+public class JiraGamificadoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JiraGamificadoApplication.class, args);
+    }
+}
