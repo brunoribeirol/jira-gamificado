99c32059bccf1bed709bba2fa0d651211efffee4
vinipetribu <132365613+vinipetribu@users.noreply.github.com>
2025-06-03 03:14:02 -0300

    Update MemberTeamServiceImpl.java

--- application/src/main/java/com/cesar/school/application/teamsmembers/MemberTeamServiceImpl.java
+++ application/src/main/java/com/cesar/school/application/teamsmembers/MemberTeamServiceImpl.java
@@ -20,7 +20,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberTeamServiceImpl implements MemberTeamService, MemberService, TeamService {
 
     private final TeamRepository teamRepository;
